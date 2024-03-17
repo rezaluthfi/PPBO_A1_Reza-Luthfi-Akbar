d9fdbfbc782836292db9f0be74a235b996ef891d
reza <102231043+rezaluthfi@users.noreply.github.com>
2024-03-17 16:51:15 +0700

    add pertemuan 5

---
+++ Pertemuan 5_Reza Luthfi Akbar/Barang.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Barang {
+    private String namaBarang;
+    private int hargaBarang, jumlahBarang;
+
+    public Barang(String namaBarang, int hargaBarang, int jumlahBarang) {
+        this.namaBarang = namaBarang;
+        this.hargaBarang = hargaBarang;
+        this.jumlahBarang = jumlahBarang;
+    }
+
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    public int getHargaBarang() {
+        return hargaBarang;
+    }
+
+    public int getJumlahBarang() {
+        return jumlahBarang;
+    }
+
+}
